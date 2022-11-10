06866ee0bb3dfcbe9769248f163c9cedf68ca159
widia-a <wwiidiiaa@gmail.com>
2022-11-10 14:41:56 +0700

    add soal 1

---
+++ src/main/java/com/bcafinance/fundamental/day4/Soal2.java
@@ -0,0 +1,13 @@
+package com.bcafinance.fundamental.day4;
+/*
+Created by IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IC-222.4345.14, built on October 5, 2022
+@Author widia a.k.a. Widia
+Created on 10/11/2022 14:36
+Last modified on 14:36
+Version 1.0
+*/
+
+
+public class Soal2 {
+}
